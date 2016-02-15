fa0b1db4f1824575e19bef79574a0e48258e26f5
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-15 15:02:18 -0600

    create seed file

--- movie_zoo/db/seeds.rb
+++ movie_zoo/db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+  User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, trusted: false)
+end
+
+10.times do
+  User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, trusted: true)
+end
+
+50.times do
+  Movie.create(title: Faker::Book.title, summary: Faker::Lorem.paragraph, year: 2000)
+end
+
+30.times do
+  Review.create(user_id: rand(0..50), movie_id: rand(0..50), body: Faker::Lorem.paragraph, rating: rand(1..5))
+end
+
+20.times do
+  Genre.create(title: Faker::Book.genre)
+end
+
+30.times do
+  Classification.create(movie_id: rand(0..50), genre_id(rand(0..20)))
+end
+
+50.times do
+  movies.comments.create(body: Faker::Lorem, user_id(rand(0..50)))
+  reviews.comments.create(body: Faker::Lorem, user_id(rand(0..50)))
+end
