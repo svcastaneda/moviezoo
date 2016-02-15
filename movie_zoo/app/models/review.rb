46ef613638e4f085f8e5096be8e3500dbd4dd801
Bob Dorff & Sabrina Unrein <pair+bobdorff.sunrein@devbootcamp.com>
2016-02-15 16:01:22 -0600

    complete review model

---
+++ movie_zoo/app/models/review.rb
@@ -0,0 +1,16 @@
+class Review < ActiveRecord::Base
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- movie_zoo/config/routes.rb
+++ movie_zoo/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :movies, except: [:destroy, :edit]
 
   resources :pops, only: [:create, :patch]
+
+  resources :reviews, except: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
