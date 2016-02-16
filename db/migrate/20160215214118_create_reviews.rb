a3622a12f1f58c58e62798f00b6833b061aa6786
bobdorff <robertdorff@gmail.com>
2016-02-15 22:42:51 -0600

    complete basic review model tests

--- db/migrate/20160215214118_create_reviews.rb
+++ db/migrate/20160215214118_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.integer :user_id
       t.integer :movie_id
-      t.string :body
+      t.string  :body
       t.integer :rating
      
 
